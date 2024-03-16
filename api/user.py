021017eb63793e87184de763a272bb540488c0fe
BayInl <dry330500@gmail.com>
2024-03-16 10:58:19 +0800

    Update Python files and configuration files

--- api/user.py
+++ api/user.py
@@ -32,6 +32,7 @@ async def register(avatar:UploadFile,username: str = Form(...), password: str = 
     return CommonResponse.success(Token(access_token=await create_access_token(data={"sub": user.username}), token_type="Bearer").to_json())
 
 
+
 @userAPI.post("/login")
 async def login(form_data: OAuth2PasswordRequestForm = Depends()) -> Token:
     user = await authenticate_user(form_data.username, form_data.password)
